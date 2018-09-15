ff6aa35beefd51d88b028cc996c2ea056c9ae607
samsam <sam.ahmadi34@gmail.com>
2018-09-15 19:37:54 +0430

    add slider section

--- js/main.js
+++ js/main.js
@@ -60,4 +60,55 @@ $(document).ready(function () {
     checkWidthWindow();
   })
 
+
+
+  // carousle slider 
+
+
+      let slideCount = $(".homez-carousle-items .item").length;
+      $(".homez-carousle-items .counter").html(`${slideCount} / <span>1</span>`)
+      function nextSlide(){
+        let activeSlide = $('.homez-carousle-items .item.active');
+        let numCount = $(".homez-carousle-items .counter span").text();
+        let slide;
+        activeSlide.removeClass("active");
+        if(activeSlide.next(".homez-carousle-items .item").length !=0 ){
+          slide = activeSlide.next();
+          numCount++;
+        }else{
+          slide = $(".homez-carousle-items .item:nth-child(2)")
+          numCount =1;
+        }
+        slide.addClass("active");
+        $(".homez-carousle-items .counter").html(`${slideCount} / <span>${numCount}</span>`)
+      }
+  
+      function prevSlide(){
+        let activeSlide = $('.homez-carousle-items .item.active');
+        let numCount = $(".homez-carousle-items .counter span").text();
+        let slide;
+        activeSlide.removeClass("active");
+        if(activeSlide.prev(".homez-carousle-items .item").length !=0 ){
+          slide = activeSlide.prev();
+          numCount--
+        }else{
+          slide = $(".homez-carousle-items .item:last-child")
+          numCount = slideCount;
+        }
+        slide.addClass("active");
+        $(".homez-carousle-items .counter").html(`${slideCount} / <span>${numCount}</span>`)
+      }
+  
+      
+          $("#next-slide").on("click",function(e){
+            nextSlide();
+          })
+      
+      
+          $("#prev-slide").on("click",function(e){
+            prevSlide();
+          })
+  // document ready
 })
+
+
